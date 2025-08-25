002cac2b78f0b7dd7680651eb8b505dd47e9fe6a
Leonard Hernandez <leonardhernandez02@gmail.com>
2025-08-24 22:02:08 -0500

    working on accounts page

--- src/app/finco-front/pages/accounts/accounts-list-page/accounts-list-page.component.ts
+++ src/app/finco-front/pages/accounts/accounts-list-page/accounts-list-page.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { AccountService } from '../../../../account/service/account.service';
+import { TransactionService } from '../../../../transaction/services/transaction.service';
+import { ExchangeRateService } from '../../../../shared/services/exchange-rate.service';
+import { Router } from '@angular/router';
+import { TransactionChartOptions, TransactionFilter, TransactionResponse } from '../../../../transaction/interface/transaction';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { TransactionChartComponent } from '../../../../transaction/components/transaction-chart/transaction-chart.component';
+import { TransactionRangesButtonsComponent } from '../../../../transaction/components/transaction-ranges-buttons/transaction-ranges-buttons.component';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-accounts-list-page',
-  imports: [],
+  imports: [TransactionChartComponent, TransactionRangesButtonsComponent],
   templateUrl: './accounts-list-page.component.html'
 })
 export class AccountsListPageComponent {
+
+  accountService = inject(AccountService);
+  transactionsService = inject(TransactionService);
+  exchangeService = inject(ExchangeRateService);
+  router = inject(Router);
+  totalTransactions = signal<number>(0);
+
+  filter = signal<TransactionFilter>({
+    pagination: {
+      page: 0,
+      size: 10,
+      sortBy: 'date',
+      sortDirection: 'asc',
+    }
+  });
+
+  transactions = rxResource({
+    request: () => this.filter(),
+    loader: () => this.transactionsService.getTransactions(this.filter()).pipe(
+      map((response: TransactionResponse) => {
+        
+        this.totalTransactions.set(response.page.totalElements);
+        if(this.totalTransactions() === 0){
+          this.router.navigate(['/accounts']);
+        }
+        return response.content.map((transaction) => {
+          transaction.amount = this.exchangeService.convert(transaction.account.currency, transaction.amount);
+          return transaction;
+        });
+      })
+    )
+  });
+
+  transactionChartOptions = computed<TransactionChartOptions>(() => {
+    return {
+      transactions: this.transactions.value()!,
+      splitBy: 'account',
+      limitSeries: 5
+    };
+  });
+
+  updateFilter(transactions: number) {
+    this.filter.set({
+      pagination: {
+        page: 0,
+        size: transactions,
+        sortBy: 'date',
+        sortDirection: 'desc',
+      }
+    });
+  }
 
 }
